af6ec081d10ba9131ffc1bdfc32f37225cbce9ec
Ilyas Akturan <ilyas.akturan@kolaybi.com>
2022-11-27 12:30:37 +0300

    Refactor chats page

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -2,19 +2,26 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import { BackendUrlConstant } from '../constants/backendUrlConstant';
 import { ActionTypes } from '../enums/actionType';
 import { baseQuery } from './api';
+import { Chat, User } from './models';
 
 export const chatApi = createApi({
   reducerPath: 'chat',
   baseQuery,
-  tagTypes: ['chat'],
+  tagTypes: ['Chat', 'User'],
   endpoints: (builder) => ({
-    chats: builder.query({
+    chats: builder.query<Chat, void>({
       query: () => ({
         url: BackendUrlConstant.GET_CHATS,
+        method: ActionTypes.GET,
+      }),
+    }),
+    chat: builder.query<User, number>({
+      query: (id) => ({
+        url: `${BackendUrlConstant.GET_CHATS}/${id}`,
         method: ActionTypes.GET,
       }),
     }),
   }),
 });
 
-export const { useChatsQuery } = chatApi;
+export const { useChatsQuery, useChatQuery } = chatApi;
